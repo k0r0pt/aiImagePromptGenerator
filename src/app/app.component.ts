196c6028767877b284434dbc426f72601c63abe4
Sudipto Sarkar <sudiptosarkar@visioplanet.org>
2025-06-28 15:40:08 +0530

    Updates for packages
    * Upgraded angular
    * Upgraded all applicable packages
    * Upgraded to new angular build system
    * Upgraded to new angular expressions
    * Updated dependabot frequency
    * Added more dependabot config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { PromptGenComponent } from './prompt-gen/prompt-gen.component';
+import { SettingsSpecifierComponent } from './settings-specifier/settings-specifier.component';
+import { LlmPromptGenComponent } from './llm-prompt-gen/llm-prompt-gen.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [MatDividerModule, PromptGenComponent, SettingsSpecifierComponent, LlmPromptGenComponent, MatIconModule]
 })
 export class AppComponent {
   title = 'k0r0pt-ai-image-prompt-gen'
@@ -13,7 +18,7 @@ export class AppComponent {
   origin = ''
   runningOnServer = false
 
-  constructor(private domSanitizer: DomSanitizer, private matIconRegistry: MatIconRegistry) {
+  constructor(private readonly domSanitizer: DomSanitizer, private readonly matIconRegistry: MatIconRegistry) {
     this.matIconRegistry.addSvgIcon("github", this.domSanitizer.bypassSecurityTrustResourceUrl(this.githubLogoUrl));
     this.origin = window.location.origin
     this.runningOnServer = !['localhost', '127.0.0.1'].includes(this.origin)
--- src/app/prompt-gen/prompt-gen.component.ts
+++ src/app/prompt-gen/prompt-gen.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { GenOptsService } from '../services/gen-opts.service';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-prompt-gen',
-  templateUrl: './prompt-gen.component.html',
-  styleUrl: './prompt-gen.component.scss'
+    selector: 'app-prompt-gen',
+    templateUrl: './prompt-gen.component.html',
+    styleUrl: './prompt-gen.component.scss',
+    imports: [MatDividerModule, FormsModule, MatFormFieldModule, MatExpansionModule, MatDividerModule, MatButtonModule, MatInputModule]
 })
 export class PromptGenComponent {
 
